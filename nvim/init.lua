7bed699f4c6b4a8cf5fcc56d3df1d1f297e08ead
palilo815 <hyunsoo1397@gmail.com>
2022-06-14 23:31:52 +0900

    lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,11 +4,11 @@ require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
 require "user.snippet"
--- require "user.lsp"
+require "user.lsp"
 require "user.treesitter"
 require "user.autopairs"
 -- require "user.language.cpp"
 
--- require("clangd_extensions").setup()
--- require('rust-tools').setup({})
+require("clangd_extensions").setup()
+require('rust-tools').setup({})
 
